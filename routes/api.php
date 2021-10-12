2b3a72ebbfaa8501d6c2e9c788c7aadc3a5c94f6
Fachrizal Rifahdi <fachrizalrifahdi@gmail.com>
2021-10-12 17:04:47 +0800

    Add JWT

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AlbumController;
 use App\Http\Controllers\Api\FotoController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,15 +21,30 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Route::resource('albums', AlbumController::class);
-Route::get('/albums', [AlbumController::class, 'index']);
-Route::post('/albums/store', [AlbumController::class, 'store']);
-Route::get('/album/{id}', [AlbumController::class, 'show']);
-Route::put('/album/{id}/update', [AlbumController::class, 'update']);
-Route::delete('/album/{id}/destroy', [AlbumController::class, 'destroy']);
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
 
-Route::get('/fotos', [FotoController::class, 'index']);
-Route::post('/foto/store', [FotoController::class, 'store']);
-Route::get('/foto/{id}', [FotoController::class, 'show']);
-Route::put('/foto/{id}/update', [FotoController::class, 'update']);
-Route::delete('/foto/{id}/destroy', [FotoController::class, 'destroy']);
+], function ($router) {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::get('/user-profile', [AuthController::class, 'userProfile']);    
+});
+
+Route::middleware(['auth:api'])->group(function () {
+    Route::get('/albums', [AlbumController::class, 'index']);
+});
+    Route::post('/albums/store', [AlbumController::class, 'store']);
+    Route::get('/album/{id}', [AlbumController::class, 'show']);
+    Route::put('/album/{id}/update', [AlbumController::class, 'update']);
+    Route::delete('/album/{id}/destroy', [AlbumController::class, 'destroy']);
+
+    Route::get('/fotos', [FotoController::class, 'index']);
+    Route::post('/foto/store', [FotoController::class, 'store']);
+    Route::get('/foto/{id}', [FotoController::class, 'show']);
+    Route::put('/foto/{id}/update', [FotoController::class, 'update']);
+    Route::delete('/foto/{id}/destroy', [FotoController::class, 'destroy']);
+
+// Route::resource('albums', AlbumController::class);
